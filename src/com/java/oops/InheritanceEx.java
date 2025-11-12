6479917a30332f14d520b899841a8882a204e10c
Punnaiah Chowdary Gorantla <pcatgothenburg@gmail.com>
2025-11-12 16:06:18 +0100

    Initial commit

---
+++ src/com/java/oops/InheritanceEx.java
@@ -0,0 +1,27 @@
+package com.java.oops;
+
+// Single Inheritance Example. Dog inherits Animal. Dog is a Animal. Is-a Relationship.
+//It promotes code reusability.
+class Animal {
+    void eat() {
+        System.out.println("Eating...");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Barking...");
+    }
+}
+
+public class InheritanceEx {
+    void main() {
+        try {
+            Dog d = new Dog();
+            d.eat();  // inherited from Animal
+            d.bark(); // from Dog
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
