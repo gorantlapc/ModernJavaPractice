f251c39e89174e4f26234b1b5b4645da14a01192
Punnaiah Chowdary Gorantla <pcatgothenburg@gmail.com>
2025-11-18 16:25:55 +0100

    Synchronized keyword example

---
+++ src/com/java/threads/RaceConditionExample.java
@@ -0,0 +1,37 @@
+package com.java.threads;
+
+//synchronized keyword is used to control access to a shared resource by multiple threads.
+//It ensures that only one thread at a time can execute a block of code or a method that’s marked as synchronized.
+class Counter {
+    private int count = 0;
+
+    public  void increment() {
+        count++; // Not thread-safe
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+public class RaceConditionExample {
+    static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) counter.increment();
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) counter.increment();
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println("Final count: " + counter.getCount());
+    }
+}
+
